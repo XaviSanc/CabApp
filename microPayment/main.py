9340e76e6ca12bb5c66083916e0a67a8ac010db0
XaviSanc <xaviersancort@gmail.com>
2025-03-31 19:58:39 +0200

    folder structure and docker set up

---
+++ microPayment/main.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, FastAPI
+import uvicorn
+
+
+app = FastAPI(
+    title="microPayment",
+    docs_url="/docs",
+)
+
+router = APIRouter(prefix="/api")
+
+
+@router.get("/health")
+def health_check():
+    return {"message": "I'm alive"}
+
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8002)
